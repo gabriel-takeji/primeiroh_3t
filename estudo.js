688366d3ac57e893e2f58571908428f4a70b85ac
gabriel-takeji <106189534+gabriel-takeji@users.noreply.github.com>
2022-10-04 16:52:04 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -2,9 +2,34 @@
 //console.log("");
 //document.write("");
 
-let tabuada = 2
-let nome = "Gabriel Takeji"
-function meChame(){
-    document.write ("<h1>Tabuada</h1>")
+var tabuada = 8;
 
+function tabuada(nome){
+    document.write("<h1>Tabuada do " + tabuada + "</h1>");
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br");
+    document.write("Feita por " + nome);
+    
 }
+
+function escreva(){
+   for(var i = 1; i < 11; i++){
+
+      document.write(tabuada + " x " + i + " = "+(tabuada*i)+"<br>");
+
+   }
+}
+
+function quadrado(){
+      for(var i = 2; i < 21; i++)
+      document.write("O quadrado de " + i + " é " + (i*i)+ "<br>")
+}
+
